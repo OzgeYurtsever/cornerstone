0af63aeae79b1714106a4637206b3d649224575a
Ozge Yurtsever <ozge@BDS-C02GM1E0Q05N.local>
2023-11-15 23:50:12 -0800

    Add consoles and build dist

--- src/rendering/initializeRenderCanvas.js
+++ src/rendering/initializeRenderCanvas.js
@@ -9,21 +9,28 @@
 export default function (enabledElement, image) {
   const renderCanvas = enabledElement.renderingTools.renderCanvas;
 
+  console.log("----> image");
+  console.log(" ---> image.width", image.width);
+  console.log(" ---> image.height", image.height);
+  
   // Resize the canvas
   renderCanvas.width = image.width;
   renderCanvas.height = image.height;
-
+  
   const canvasContext = renderCanvas.getContext('2d', {
     desynchronized: true
   });
-
+  
   // NOTE - we need to fill the render canvas with white pixels since we
   // control the luminance using the alpha channel to improve rendering performance.
   canvasContext.fillStyle = 'white';
   canvasContext.fillRect(0, 0, renderCanvas.width, renderCanvas.height);
-
+  
   const renderCanvasData = canvasContext.getImageData(0, 0, image.width, image.height);
-
+  
+  console.log(' ----> canvas context');
+  console.log(canvasContext);
+  
   enabledElement.renderingTools.renderCanvasContext = canvasContext;
   enabledElement.renderingTools.renderCanvasData = renderCanvasData;
 }
